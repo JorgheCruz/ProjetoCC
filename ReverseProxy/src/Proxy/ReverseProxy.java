23c343eb695b0824b059911c3a22860b92c0e3a7
Jorge <utilizador@MacBook-Pro-de-Utilizador.local>
2018-05-15 15:47:11 +0100

    Reverse Proxy

--- ReverseProxy/src/Proxy/ReverseProxy.java
+++ ReverseProxy/src/Proxy/ReverseProxy.java
@@ -1,5 +1,8 @@
 package Proxy;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +15,8 @@ package Proxy;
  */
 public class ReverseProxy {
     
+    public static void main(String[] args) throws SocketException, IOException, InterruptedException {
+        Monitor monitor = new Monitor();
+        monitor.start("239.8.8.8");
+    }
 }
