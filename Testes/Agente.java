0cf39c4ee58e96ac2ad6c236cbaa47d6c086ce6a
NeverMana <jmsd.jose.dias@gmail.com>
2018-05-28 22:06:28 +0100

    alguma documentaçao

--- Testes/Agente.java
+++ Testes/Agente.java
@@ -8,88 +8,103 @@ import static java.lang.Thread.sleep;
 import java.lang.management.ManagementFactory;
 import com.sun.management.OperatingSystemMXBean;
 import java.net.UnknownHostException;
-
 /**
- *
- * @author treishy
- */
+
+A classe Agente guarda várias componentes do Estado de um servidor e 
+é responsável por responder ao probing Multicast feito pelo Monitor 
+com as mesmas.
+
+  @author	Grupo 28
+*/
 public class Agente {
-   
+
+    /** Percentagem de ram disponível*/
 	private double ram;
-        private double cpu;
+	/** Percentagem de CPU disponível*/
+    private double cpu;
+    /** Tempo de delay da mensagem*/
 	private long delayTime;
+	/** Memória total do servidor*/
 	private final double totalMemory;
+	/** IP local*/
 	private final String localIP;
 
+	/** Construtor da classe*/
     public Agente() throws UnknownHostException {
 
-	this.localIP = InetAddress.getLocalHost().getHostAddress();
+		this.localIP = InetAddress.getLocalHost().getHostAddress();
         OperatingSystemMXBean os = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-	this.totalMemory = (double) (os.getTotalPhysicalMemorySize() / (1024*1024));
+		this.totalMemory = (double) (os.getTotalPhysicalMemorySize() / (1024*1024));
     }	
-        
+    /** Método para atualizar Estados*/
     public void updateEstado() {
         
-	int MB = 1024*1024;
+		int MB = 1024*1024;
         OperatingSystemMXBean os = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-       
-	/** Calculate free CPU */
+    	   
+		/** Calculo de CPU livre */
         int cores = os.getAvailableProcessors();
-	double loadAvg = (double) os.getSystemLoadAverage();	
-	cpu = (loadAvg / cores) * 100;
-	cpu = 100 - (Math.round(cpu * 100) / 100.0d);
+		double loadAvg = (double) os.getSystemLoadAverage();	
+		cpu = (loadAvg / cores) * 100;
+		cpu = 100 - (Math.round(cpu * 100) / 100.0d);
 
-	/** Calculate free RAM */
+		/** Calculo da RAM livre*/
         double usedMemory = (double) ((this.totalMemory - (os.getFreePhysicalMemorySize() / MB)));
         ram = (usedMemory / this.totalMemory) * 100;
         ram = 100 - (Math.round(ram * 100) / 100.0d);
     }
 
+    /** 
+    Conversão da informação do objeto para uma string
+	@param serverID id do servidor
+	@return string com id do servidor, ram, cpu e tempo de delay das mensagens
+    */
     public String toString(String serverID) {
 
-	String resposta = (serverID + "/" + this.ram + "/" + this.cpu + "/" + this.delayTime);
+		String resposta = (serverID + "/" + this.ram + "/" + this.cpu + "/" + this.delayTime);
         return resposta;
     }
 
+
+    /** Método main da classe*/
     public static void main (String args[]) throws SocketException, IOException, InterruptedException {
         
-	Agente agente = new Agente();
+		Agente agente = new Agente();
         String serverID = agente.localIP + ":" + args[0];
-	Cypher cypher = new Cypher();
+		Cypher cypher = new Cypher();
 
-	/** Join Multicast Socket in IP 239.8.8.8 and Port 8888 */
+		/** Join Multicast Socket in IP 239.8.8.8 and Port 8888 */
         MulticastSocket socketToReceive = new MulticastSocket(8888);
-	InetAddress group = InetAddress.getByName("239.8.8.8");
-	socketToReceive.joinGroup(group);
+		InetAddress group = InetAddress.getByName("239.8.8.8");
+		socketToReceive.joinGroup(group);
 
-	DatagramSocket socketToSend = new DatagramSocket();
+		DatagramSocket socketToSend = new DatagramSocket();
 	
-	byte[] received = new byte[1024];
-	DatagramPacket requestPacket = new DatagramPacket(received, received.length);
-	DatagramPacket answerPacket; 
+		byte[] received = new byte[1024];
+		DatagramPacket requestPacket = new DatagramPacket(received, received.length);
+		DatagramPacket answerPacket; 
        
        	while (true) {
 
         	socketToReceive.receive(requestPacket);
-		String requestMessage = cypher.checkMessage(new String(requestPacket.getData(), 0, requestPacket.getLength()));
+			String requestMessage = cypher.checkMessage(new String(requestPacket.getData(), 0, requestPacket.getLength()));
 		
-		/** Check request integrity and authentication */
-		if (requestMessage != null) {
+			/** Verificar autenticação e integrĩdade */
+			if (requestMessage != null) {
 
-			/** Delay answer between 0 and 10 ms */
-			agente.delayTime = (long) (Math.random() * 10);
-			sleep(agente.delayTime);
+				/** Atrasar a resposta entre 0 e 10 ms */
+				agente.delayTime = (long) (Math.random() * 10);
+				sleep(agente.delayTime);
 
-			agente.updateEstado();
-			String answer = agente.toString(serverID);
-			String answerHash = cypher.encrypt(answer);
-			String msg = answer + "\n" + answerHash;
+				agente.updateEstado();
+				String answer = agente.toString(serverID);
+				String answerHash = cypher.encrypt(answer);
+				String msg = answer + "\n" + answerHash;
 
-			/** Prepare and send Datagram Packet with server info */
-			answerPacket = new DatagramPacket(msg.getBytes(), msg.length(), requestPacket.getAddress(), requestPacket.getPort());
-			socketToSend.send(answerPacket);
+				/** Preparar e enviar o Datagram Packet com a informação do servidor */
+				answerPacket = new DatagramPacket(msg.getBytes(), msg.length(), requestPacket.getAddress(), requestPacket.getPort());
+				socketToSend.send(answerPacket);
+			}
 		}
-	}
-        
     }
 }
--- Testes/Cypher.java
+++ Testes/Cypher.java
@@ -5,18 +5,27 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
-
+/**
+ Esta classe foi desenvolvida com o objetivo de verificar a integridade e autenticação de origem 
+ das mensagens trocadas entre o Monitor e os vários Agentes com uma assinatura digital simétrica.
+ Contém as funções de cifrar texto e de averiguar as mensagens recebidas.
+ @author	Grupo 28
+*/
 public class Cypher {
 
 	private static String secretKey = "cc1718";
+	/**
+	 Função que aplica o inicio do algoritmo HMAC á mensagem passada como argumento.
+	 @param message mensagem que será encriptada
+	 @return mensagem encriptada.
+	 */
+    public String encrypt(String message) {
 
-    	public String encrypt(String message) {
+    	String hash = null;
 
-        	String hash = null;
-
-        	try {
-            		SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
-            		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
             
 			sha256_HMAC.init(keySpec);
 			byte[] encodedBytes = Base64.getEncoder().encode(sha256_HMAC.doFinal(message.getBytes()));
@@ -29,9 +38,15 @@ public class Cypher {
             		System.out.println("The key provided is not valid!\n");
         	}
 
-        	return hash;
-    	}
+        return hash;
+    }
 
+    /** 
+     Função que recebe a mensagem enviada do exterior, 
+     e verifica a integridade e autenticidade da mesma.
+     @param message mensagem enviada do exterior 
+     @return mensagem descodificada
+    */
 	public String checkMessage(String message) {
 		
 		String finalMessage = null;
